ed7b77e37898493a30491c170e854a2ebe1c6e3b
Wolfgang Doll <wolfgang.doll@web.de>
2017-08-05 14:31:27 +0200

    bugfix 3 in conf.py for read the docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,7 @@ import sphinx_rtd_theme
 extensions = ['sphinx.ext.autodoc']
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['source/_templates']
+templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -43,7 +43,7 @@ templates_path = ['source/_templates']
 source_suffix = '.rst'
 
 # The master toctree document.
-master_doc = 'source/index'
+master_doc = 'index'
 
 # General information about the project.
 project = 'WebTool3'
@@ -92,7 +92,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['source/_static']
+html_static_path = ['_static']
 
 
 # -- Options for HTMLHelp output ------------------------------------------
