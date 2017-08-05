10e45a6766e2f5b325fb654f3af4f4241bf1cd29
Wolfgang Doll <wolfgang.doll@web.de>
2017-08-05 14:37:04 +0200

    bugfix 4 in conf.py for read the docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../webtool'))
+sys.path.insert(0, os.path.abspath('../../webtool'))
 import sphinx_rtd_theme
 
 # -- General configuration ------------------------------------------------
