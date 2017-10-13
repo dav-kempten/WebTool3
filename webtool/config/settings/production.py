c5cfdc2393ea3ac0741d0b5c35f9138246a50926
Wolfgang Doll <wolfgang.doll@web.de>
2017-10-13 18:41:39 +0200

    introduce proxy parameter

--- webtool/config/settings/production.py
+++ webtool/config/settings/production.py
@@ -2,6 +2,9 @@ from .base import *
 
 DEBUG = False
 
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 ALLOWED_HOSTS = ['webtool.dav-kempten.de', '46.252.16.44']
 
 STATICFILES_DIRS = [
