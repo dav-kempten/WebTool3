e0d1a0a9fe16dd446b901e26b2e81645f62e5284
Wolfgang Doll <wolfgang.doll@web.de>
2017-10-13 15:48:30 +0200

    small fix for collect static

--- webtool/config/settings/base.py
+++ webtool/config/settings/base.py
@@ -22,7 +22,7 @@ def get_env(name):
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # See https://docs.djangoproject.com/en/1.11/ref/contrib/sites/
 SITE_ID = 1
