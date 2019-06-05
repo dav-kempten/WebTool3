23674096746b891984b7fc9d86802015ef43adde
Wolfgang Doll <wolfgang.doll@web.de>
2019-06-05 04:53:37 +0200

    prototypical instruction-list and instruction-detail view

--- webtool/frontend/src/app/instruction/instruction-detail/instruction-detail.component.ts
+++ webtool/frontend/src/app/instruction/instruction-detail/instruction-detail.component.ts
@@ -1,7 +1,13 @@
-import {Observable} from 'rxjs';
+import {Observable, of} from 'rxjs';
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {select, Store} from '@ngrx/store';
 import {AppState, selectRouterDetailId} from '../../app.state';
+import {Instruction} from '../../core/store/instruction.model';
+import {concatAll, filter, flatMap, map, switchMap, tap} from 'rxjs/operators';
+import {getInstructionById} from '../../core/store/instruction.selectors';
+import {RequestInstruction} from '../../core/store/instruction.actions';
+import {getEventById, getEventsByIds} from "../../core/store/event.selectors";
+import {Event} from '../../model/event';
 
 @Component({
   selector: 'avk-instruction-detail',
@@ -11,11 +17,31 @@ import {AppState, selectRouterDetailId} from '../../app.state';
 export class InstructionDetailComponent implements OnInit, OnDestroy {
 
   instructionId$: Observable<number>;
+  instruction$: Observable<Instruction>;
+  eventIds$: Observable<number[]>;
+  events$: Observable<Event[]>;
 
   constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
-    this.instructionId$ = this.store.pipe(select(selectRouterDetailId));
+    this.instructionId$ = this.store.select(selectRouterDetailId);
+    this.instruction$ = this.instructionId$.pipe(
+      flatMap(id => this.store.pipe(
+          select(getInstructionById(id)),
+          tap(instruction => {
+            if (!instruction) {
+              this.store.dispatch(new RequestInstruction({id}));
+            }
+          })
+        )
+      )
+    );
+    this.eventIds$ = this.instruction$.pipe(
+      map(instruction => [instruction.instructionId, ...instruction.meetingIds])
+    );
+    this.events$ = this.eventIds$.pipe(
+      flatMap(eventIds => this.store.select(getEventsByIds(eventIds)))
+    );
   }
 
   ngOnDestroy(): void {}
