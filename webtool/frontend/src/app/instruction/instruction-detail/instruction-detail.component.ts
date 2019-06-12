71da32ab4528568f6c3a47bb5da0000536044001
JoJo <jojobueddi@web.de>
2019-06-12 08:22:12 +0200

    reformat typescript code and change input fields on instruction

--- webtool/frontend/src/app/instruction/instruction-detail/instruction-detail.component.ts
+++ webtool/frontend/src/app/instruction/instruction-detail/instruction-detail.component.ts
@@ -2,7 +2,7 @@ import {Observable} from 'rxjs';
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {select, Store} from '@ngrx/store';
 import {AppState, selectRouterDetailId} from '../../app.state';
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormArray, FormControl, FormGroup} from "@angular/forms";
 import {NameListRequested} from "../../core/store/name.actions";
 import {ValuesRequested} from "../../core/store/value.actions";
 import {CalendarRequested} from "../../core/store/calendar.actions";
@@ -52,85 +52,112 @@ export class InstructionDetailComponent implements OnInit, OnDestroy {
   formState$: Observable<State>;
   authState$: Observable<User>;
 
-  guideId = new FormControl(undefined);
-  teamIds = new FormControl([]);
-  costs = new FormControl('');
-  revenue = new FormControl('');
-  description = new FormControl('');
-  notes = new FormControl('');
-  reference = new FormControl('');
-  stateId = new FormControl('');
-  topicId = new FormControl('');
+  display: boolean = false;
+
+  eventArray: FormArray;
+
+  tours: Tour[] = [];
+  userValState: number = 0;
+
+  /* FormControls for Instruction & Event */
+  id = new FormControl('');
+
+  /* FormControls for Event */
   title = new FormControl('');
   name = new FormControl('');
-  equipmentIds = new FormControl('');
-  qualificationIds = new FormControl('');
-  minQuantity = new FormControl('');
-  maxQuantity = new FormControl('');
-  distance = new FormControl('');
-  equipmentService = new FormControl('');
-  tourcosts = new FormControl('');
-  costsctr = new FormControl('');
-  costsname = new FormControl('');
-  extracosts = new FormControl('');
-  admission = new FormControl('');
-  deposit = new FormControl('');
-  extraCharges = new FormControl('');
-  startdate = new FormControl('');
-  enddate = new FormControl('');
-  datetype = new FormControl('');
-  location = new FormControl('');
-  multisingle = new FormControl('');
+  description = new FormControl('');
+  startDate = new FormControl('');
+  startTime = new FormControl('');
   approximateId = new FormControl('');
-  time = new FormControl('');
-  meetingIds = new FormControl('');
+  endDate = new FormControl('');
+  endTime = new FormControl('');
+  rendevous = new FormControl('');
+  location = new FormControl('');
+  reservationService = new FormControl('');
+  source = new FormControl('');
+  link = new FormControl('');
+  map = new FormControl('');
+  distal = new FormControl('');
+  distance = new FormControl('');
+  publicTransport = new FormControl('');
+  shuttleService = new FormControl('');
+
+  /* FormControls for Instruction */
+  reference = new FormControl('');
+  guideId = new FormControl(undefined);
+  teamIds = new FormControl([]);
+  topicId = new FormControl('');
   lowEmissionAdventure = new FormControl('');
   ladiesOnly = new FormControl('');
   isSpecial = new FormControl('');
+  categoryId = new FormControl('');
+  qualificationIds = new FormControl('');
+  preconditions = new FormControl('');
+  equipmentIds = new FormControl('');
+  miscEquipment = new FormControl('');
+  equipmentService = new FormControl('');
+  admission = new FormControl('');
+  advances = new FormControl('');
+  advancesInfo = new FormControl('');
+  extraCharges = new FormControl('');
+  extraChargesInfo = new FormControl('');
+  minQuantity = new FormControl('');
+  maxQuantity = new FormControl('');
+  curQuantity = new FormControl('');
+  stateId = new FormControl('');
+  instructionId = new FormControl('');
+  meetingIds = new FormControl([]);
+
+  eventForm = new FormGroup({
+    id: this.id,
+    title: this.title,
+    name: this.name,
+    description: this.description,
+    startDate: this.startDate,
+    startTime: this.startTime,
+    approximateId: this.approximateId,
+    endDate: this.endDate,
+    endTime: this.endTime,
+    rendevous: this.rendevous,
+    location: this.location,
+    reservationService: this.reservationService,
+    source: this.source,
+    link: this.link,
+    map: this.map,
+    distal: this.distal,
+    distance: this.distance,
+    publicTransport: this.publicTransport,
+    shuttleService: this.shuttleService,
+  });
 
 
   instructionForm = new FormGroup({
+    id: this.id,
+    reference: this.reference,
     guideId: this.guideId,
     teamIds: this.teamIds,
-    costs: this.costs,
-    revenue: this.revenue,
-    description: this.description,
-    notes: this.notes,
-    reference: this.reference,
-    stateId: this.stateId,
     topicId: this.topicId,
-    title: this.title,
-    name: this.name,
-    equipmentIds: this.equipmentIds,
-    qualificationIds: this.qualificationIds,
-    minQuantity: this.minQuantity,
-    maxQuantity: this.maxQuantity,
-    distance: this.distance,
-    equipmentService: this.equipmentService,
-    costsctr: this.costsctr,
-    tourcosts: this.tourcosts,
-    costsname: this.costsname,
-    extracosts: this.extracosts,
-    deposit: this.deposit,
-    extraCharges: this.extraCharges,
-    admission: this.admission,
-    startdate: this.startdate,
-    enddate: this.enddate,
-    datetype: this.datetype,
-    location: this.location,
-    multisingle: this.multisingle,
-    approximateId: this.approximateId,
-    time: this.time,
-    meetingIds: this.meetingIds,
     lowEmissionAdventure: this.lowEmissionAdventure,
     ladiesOnly: this.ladiesOnly,
-    isSpecial: this.isSpecial
+    isSpecial: this.isSpecial,
+    categoryId: this.categoryId,
+    qualificationIds: this.qualificationIds,
+    preconditions: this.preconditions,
+    equipmentIds: this.equipmentIds,
+    miscEquipment: this.miscEquipment,
+    equipmentService: this.equipmentService,
+    admission: this.admission,
+    advances: this.advances,
+    advancesInfo: this.advancesInfo,
+    extraCharges: this.extraCharges,
+    extraChargesInfo: this.extraChargesInfo,
+    minQuantity: this.minQuantity,
+    maxQuantity: this.maxQuantity,
+    curQuantity: this.curQuantity,
+    stateId: this.stateId,
+    instructionId: this.instructionId,
+    meetingIds: this.meetingIds,
   });
-
-  tours: Tour[] = [];
-  totalcostsTable: Costs[] = [];
-
-  userValState: number = 0;
 
   constructor(private store: Store<AppState>, private userService: AuthService) {
     this.store.dispatch(new NameListRequested());
@@ -181,15 +208,15 @@ export class InstructionDetailComponent implements OnInit, OnDestroy {
 
     this.events$ = this.eventIds$.pipe(
       flatMap(eventIds => this.store.select(getEventsByIds(eventIds))),
-      tap(eventIds => console.log("EventIds", eventIds)),
+      // tap(eventIds => console.log("EventIds", eventIds)),
       tap(eventIds => {
-          if (eventIds[0].startDate !== null) this.startdate.setValue(eventIds[0].startDate);
-          if (eventIds[0].endDate !== null) this.enddate.setValue(eventIds[0].endDate);
+          if (eventIds[0].startDate !== null) this.startDate.setValue(eventIds[0].startDate);
+          if (eventIds[0].endDate !== null) this.endDate.setValue(eventIds[0].endDate);
           this.title.setValue(eventIds[0].title);
           this.name.setValue(eventIds[0].name);
           this.location.setValue(eventIds[0].location);
           this.approximateId.setValue(eventIds[0].approximateId);
-          this.time.setValue(eventIds[0].startTime);
+          this.startTime.setValue(eventIds[0].startTime);
           this.description.setValue(eventIds[0].description);
       }),
       tap(eventIds => {
@@ -224,79 +251,59 @@ export class InstructionDetailComponent implements OnInit, OnDestroy {
     this.formInstruction$.subscribe((instruction: Instruction) => {
       if (instruction !== undefined) {
         this.instructionForm.setValue({
+          id: instruction.id,
+          reference: instruction.reference,
           guideId: instruction.guideId,
           teamIds: instruction.teamIds,
-          costs: '',
-          revenue: '',
-          description: '',
-          notes: '',
-          reference: instruction.reference,
-          stateId: instruction.stateId,
           topicId: instruction.topicId,
-          title: '',
-          name: '',
-          equipmentIds: instruction.equipmentIds,
-          qualificationIds: instruction.qualificationIds,
-          minQuantity: instruction.minQuantity,
-          maxQuantity: instruction.maxQuantity,
-          distance: '',
-          equipmentService: instruction.equipmentService,
-          costsctr: 0,
-          tourcosts: '',
-          costsname: '',
-          extracosts: '',
-          deposit: '',
-          extraCharges: instruction.extraCharges,
-          admission: instruction.admission,
-          startdate: '',
-          enddate: '',
-          datetype: '',
-          location: '',
-          multisingle: (instruction.meetingIds.length > 0),
-          approximateId: '',
-          time: '',
-          meetingIds: instruction.meetingIds,
           lowEmissionAdventure: instruction.lowEmissionAdventure,
           ladiesOnly: instruction.ladiesOnly,
-          isSpecial: instruction.isSpecial
+          isSpecial: instruction.isSpecial,
+          categoryId: instruction.categoryId,
+          qualificationIds: instruction.qualificationIds,
+          preconditions: instruction.preconditions,
+          equipmentIds: instruction.equipmentIds,
+          miscEquipment: instruction.miscEquipment,
+          equipmentService: instruction.equipmentService,
+          admission: instruction.admission,
+          advances: instruction.advances,
+          advancesInfo: instruction.advancesInfo,
+          extraCharges: instruction.extraCharges,
+          extraChargesInfo: instruction.extraChargesInfo,
+          minQuantity: instruction.minQuantity,
+          maxQuantity: instruction.maxQuantity,
+          curQuantity: instruction.curQuantity,
+          stateId: instruction.stateId,
+          instructionId: instruction.instructionId,
+          meetingIds: instruction.meetingIds
             });
       } else {
         this.instructionForm.setValue({
+          id: '',
+          reference: '',
           guideId: '',
           teamIds: '',
-          costs: '',
-          revenue: '',
-          description: '',
-          notes: '',
-          reference: '',
-          stateId: '',
           topicId: '',
-          title: '',
-          name: '',
-          equipmentIds: '',
-          qualificationIds: '',
-          minQuantity: '',
-          maxQuantity: '',
-          distance: '',
-          equipmentService: '',
-          costsctr: 0,
-          tourcosts: '',
-          costsname: '',
-          extracosts: '',
-          deposit: '',
-          extraCharges: '',
-          admission: '',
-          startdate: '',
-          enddate: '',
-          datetype: '',
-          location: '',
-          multisingle: '',
-          approximateId: '',
-          time: '',
-          meetingIds: '',
           lowEmissionAdventure: '',
           ladiesOnly: '',
-          isSpecial: ''
+          isSpecial: '',
+          categoryId: '',
+          qualificationIds: '',
+          preconditions: '',
+          equipmentIds: '',
+          miscEquipment: '',
+          equipmentService: '',
+          admission: '',
+          advances: '',
+          advancesInfo: '',
+          extraCharges: '',
+          extraChargesInfo: '',
+          minQuantity: '',
+          maxQuantity: '',
+          curQuantity: '',
+          stateId: '',
+          instructionId: '',
+          meetingIds: ''
         });
       }
     });
@@ -310,27 +317,8 @@ export class InstructionDetailComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {}
 
-  getTourCosts(): void {
-    if (this.costsname.value !== '' && this.tourcosts.value !== '') {
-      /* increasing counter for each cost instance */
-      let ctr = this.costsctr.value;
-      this.costsctr.setValue(++ctr);
-
-      /* list input-data directly on website */
-      let varCost: Costs = {pos: this.costsctr.value, betrag: this.tourcosts.value, beschreibung: this.costsname.value};
-      this.totalcostsTable.push(varCost);
-    }
-  }
-
-  getDateData(): void {
-    if (this.datetype.value !== '' && this.startdate.value !== '' && this.enddate.value !== '' && this.location.value !== '') {
-      let dateData: Tour = {
-        type: this.datetype.value, sdate: this.startdate.value, stime: "",
-        edate: this.enddate.value, etime: "", title: "", name: "", location: this.location.value
-      };
-
-      this.tours.push(dateData);
-    }
+  onRowSelect(event) {
+    this.display = true;
   }
 
 }
