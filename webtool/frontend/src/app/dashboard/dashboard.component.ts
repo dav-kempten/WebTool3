9ae3027bef974dc5651db78d00915a581ddc4e7b
Wolfgang Doll <wolfgang.doll@web.de>
2019-06-25 04:40:53 +0200

    dashboard login/logout prototype removed

--- webtool/frontend/src/app/dashboard/dashboard.component.ts
+++ webtool/frontend/src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {AuthService, User} from '../core/service/auth.service';
-import {Observable} from 'rxjs';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'avk-dashboard',
@@ -11,50 +7,8 @@ import {map} from 'rxjs/operators';
 })
 export class DashboardComponent implements OnInit {
 
-  userForm: FormGroup;
-  memberForm: FormGroup;
+  constructor() {}
 
-  user$: Observable<User>;
-  isLoggedIn$: Observable<boolean>;
-  isLoggedOut$: Observable<boolean>;
+  ngOnInit() {}
 
-  constructor(private fb: FormBuilder, private userService: AuthService) {
-    this.userForm = this.fb.group({
-      userName: ['Rambo-Selcher', Validators.required],
-      password: ['12345678', Validators.required]
-    });
-
-    this.memberForm = this.fb.group({
-      memberId: '008-00-123456'
-    });
-  }
-
-  ngOnInit() {
-    this.user$ = this.userService.user$;
-    this.isLoggedIn$ = this.userService.isLoggedIn$;
-    this.isLoggedOut$ = this.userService.isLoggedOut$;
-  }
-
-  userLogin() {
-    const value = this.userForm.value;
-    if (value.userName && value.password) {
-      this.userService.login(value.userName, value.password)
-        .subscribe( user => { if (!!user) {console.log(`User ${user.firstName} ${user.lastName} is logged in`); }});
-    }
-  }
-
-  memberLogin() {
-    const value = this.memberForm.value;
-    if (value.memberId) {
-      this.userService.login('', '', value.memberId)
-        .subscribe( user => console.log(`Member ${user.firstName} ${user.lastName} is logged in`));
-    }
-  }
-
-  logout() {
-    this.user$.pipe(
-      map(user => user.role)
-    ).subscribe( role => console.log(`${role} is logged out`)).unsubscribe();
-    this.userService.logout();
-  }
 }
