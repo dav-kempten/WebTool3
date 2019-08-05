c90f5a81258a18bf289e7c897f289ca151ed4d89
JoJo <jojobueddi@web.de>
2019-08-05 11:47:01 +0200

    add factory for tour-Formgroup

--- webtool/frontend/src/app/core/factories.ts
+++ webtool/frontend/src/app/core/factories.ts
@@ -2,7 +2,7 @@ import {Instruction} from "./store/instruction.model";
 import {FormControl, FormGroup} from "@angular/forms";
 import {Category, Topic} from "../model/value";
 import {Event} from "../model/event";
-import {Tour} from "../model/tour";
+import {Tour} from "./store/tour.model";
 
 export function instructionGroupFactory(instruction: Instruction): FormGroup {
   return new FormGroup({
@@ -83,5 +83,42 @@ export function eventGroupFactory(event: Event): FormGroup {
     distance: new FormControl({value: event.distance, disabled: !event.distal}),
     publicTransport: new FormControl(event.publicTransport),
     shuttleService: new FormControl(event.shuttleService)
+  });
+}
+
+export function tourGroupFactory(tour: Tour): FormGroup {
+  return new FormGroup({
+    id: new FormControl(tour.id),
+    reference: new FormControl(tour.reference),
+    categoryId: new FormControl(tour.categoryId),
+    miscCategory: new FormControl(tour.miscCategory),
+    ladiesOnly: new FormControl(tour.ladiesOnly),
+    youthOnTour: new FormControl(tour.youthOnTour),
+    deadline: new FormControl(tour.deadline),
+    preliminary: new FormControl(tour.preliminary),
+    info: new FormControl(tour.info),
+    tourstart: new FormControl(tour.tourstart),
+    tourend: new FormControl(tour.tourend),
+    portal: new FormControl(tour.portal),
+    season: new FormControl(tour.season),
+    guideId: new FormControl(tour.guideId),
+    teamIds: new FormControl(tour.teamIds),
+    preconditions: new FormControl(tour.preconditions),
+    miscEquipment: new FormControl(tour.miscEquipment),
+    admission: new FormControl((tour.admission / 100).toFixed(2)),
+    advances: new FormControl((tour.advances / 100).toFixed(2)),
+    advancesInfo: new FormControl(tour.advancesInfo),
+    extraCharges: new FormControl((tour.extraCharges / 100).toFixed(2)),
+    minQuantity: new FormControl(tour.minQuantity),
+    maxQuantity: new FormControl(tour.maxQuantity),
+    curQuantity: new FormControl(tour.curQuantity),
+    calcBudget: new FormControl(tour.calcBudget),
+    realCosts: new FormControl(tour.realCosts),
+    budgetInfo: new FormControl(tour.budgetInfo),
+    message: new FormControl(tour.message),
+    comment: new FormControl(tour.comment),
+    statedId: new FormControl(tour.stateId),
+    updated: new FormControl(tour.updated),
+    deprecated: new FormControl(tour.deprecated),
   });
 }
