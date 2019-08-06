24aedf517ec6e1a4a0030539a01d6d7e70bd9fbb
Johannes Buettner <jojo@dav-kempten.de>
2019-08-06 10:11:08 +0200

    replace marks for strings

--- webtool/frontend/src/app/tour/tour-detail/tour-detail.component.ts
+++ webtool/frontend/src/app/tour/tour-detail/tour-detail.component.ts
@@ -3,11 +3,11 @@ import {Component, OnInit} from '@angular/core';
 import {select, Store} from '@ngrx/store';
 import {FormGroup} from '@angular/forms';
 import {AppState, selectRouterDetailId} from '../../app.state';
-import {tourGroupFactory} from "../../core/factories";
-import {Tour} from "../../core/store/tour.model";
-import {NamesRequested} from "../../core/store/name.actions";
-import {ValuesRequested} from "../../core/store/value.actions";
-import {CalendarRequested} from "../../core/store/calendar.actions";
+import {tourGroupFactory} from '../../core/factories';
+import {Tour} from '../../core/store/tour.model';
+import {NamesRequested} from '../../core/store/name.actions';
+import {ValuesRequested} from '../../core/store/value.actions';
+import {CalendarRequested} from '../../core/store/calendar.actions';
 
 @Component({
   selector: 'avk-tour-detail',
@@ -23,44 +23,44 @@ export class TourDetailComponent implements OnInit {
 
   tourFrame: Tour = {
     id: 0,
-    reference: "BLA-666",
+    reference: 'BLA-666',
     categoryId: 0,
-    miscCategory: "",
+    miscCategory: '',
     ladiesOnly: false,
     youthOnTour: false,
     lowEmissionAdventure: true,
     bikeTrain: true,
     deadline: '2019-09-21',
     preliminary: '2019-09-22',
-    info: "",
+    info: '',
     tourstart: '2019-09-23',
     tourend: '2019-09-24',
-    portal: "",
-    season: "",
+    portal: '',
+    season: '',
     guideId: 384,
     teamIds: [],
     preconditionId: 0,
-    miscEquipment: "",
+    miscEquipment: '',
     admission: 0,
     advances: 0,
-    advancesInfo: "",
+    advancesInfo: '',
     extraCharges: 0,
     minQuantity: 0,
     maxQuantity: 0,
     curQuantity : 0,
     calcBudget: 0,
     realCosts: 0,
-    budgetInfo: "",
-    message: "",
-    comment: "",
+    budgetInfo: '',
+    message: '',
+    comment: '',
     stateId: 0,
-    updated: "",
+    updated: '',
     deprecated: false,
-    shortTitle: "",
-    longTitle:"",
+    shortTitle: '',
+    longTitle: '',
     equipmentIds: [],
     equipmentService: true,
-    preconditions: ""
+    preconditions: ''
   };
 
   tourForm: FormGroup = tourGroupFactory(this.tourFrame);
