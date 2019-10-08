3b2b09a53eb10a1cb9528361cc456318e3be0218
Bueddi <jojo@dav-kempten.de>
2019-10-08 10:12:13 +0200

    add tour effects & reducer to core-module

--- webtool/frontend/src/app/core/core.module.ts
+++ webtool/frontend/src/app/core/core.module.ts
@@ -39,8 +39,12 @@ import {DropdownComponent} from './dropdown/dropdown.component';
 import {reducer as eventReducer} from './store/event.reducer';
 import {reducer as instructionReducer} from './store/instruction.reducer';
 import {reducer as instructionSummaryReducer} from './store/instruction-summary.reducer';
+import {reducer as tourReducer} from './store/tour.reducer';
+import {reducer as tourSummaryReducer} from './store/tour-summary.reducer';
 import {InstructionSummaryEffects} from './store/instruction-summary.effects';
 import {InstructionEffects} from './store/instruction.effects';
+import {TourSummaryEffects} from './store/tour-summary.effects';
+import {TourEffects} from './store/tour.effects';
 import {reducer as stateReducer} from './store/state.reducer';
 import {reducer as categoryReducer} from './store/category.reducer';
 import {reducer as approximateReducer} from './store/approximate.reducer';
@@ -90,6 +94,10 @@ import {TimePipe} from './time/time.pipe';
     EffectsModule.forFeature([InstructionEffects]),
     StoreModule.forFeature('instructionSummaries', instructionSummaryReducer),
     EffectsModule.forFeature([InstructionSummaryEffects]),
+    StoreModule.forFeature('tours', tourReducer),
+    EffectsModule.forFeature([TourEffects]),
+    StoreModule.forFeature('tourSummaries', tourSummaryReducer),
+    EffectsModule.forFeature([TourSummaryEffects]),
     StoreModule.forFeature('states', stateReducer),
     StoreModule.forFeature('categories', categoryReducer),
     StoreModule.forFeature('approximates', approximateReducer),
