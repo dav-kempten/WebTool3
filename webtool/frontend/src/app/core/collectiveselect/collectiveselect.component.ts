4f333a0ed5f2a9170448c8497f80f2b05463df06
Bueddi <jojo@dav-kempten.de>
2019-10-24 08:12:55 +0200

    setup for collective component

--- webtool/frontend/src/app/core/collectiveselect/collectiveselect.component.ts
+++ webtool/frontend/src/app/core/collectiveselect/collectiveselect.component.ts
@@ -1,15 +1,147 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterContentInit,
+  AfterViewInit,
+  Component,
+  ContentChild,
+  forwardRef,
+  Input,
+  OnDestroy,
+  OnInit,
+  ViewChild
+} from '@angular/core';
+import {ControlValueAccessor, FormControl, FormControlName, FormGroup, NG_VALUE_ACCESSOR} from '@angular/forms';
+import {Dropdown} from 'primeng/dropdown';
+import {BehaviorSubject, Observable, ReplaySubject, Subject, Subscription} from 'rxjs';
+import {Collective as RawCollective} from '../../model/value';
+import {State as CollectiveState} from '../store/collective.reducer';
+import {stateValidator} from '../dropdown/dropdown.component';
+import {Store} from '@ngrx/store';
+import {AppState} from '../../app.state';
+import {getCollectiveState} from '../store/value.selectors';
+import {delay, publishReplay, refCount, takeUntil, tap} from 'rxjs/operators';
 
 @Component({
   selector: 'avk-collectiveselect',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => CollectiveselectComponent),
+      multi: true
+    }
+  ],
   templateUrl: './collectiveselect.component.html',
   styleUrls: ['./collectiveselect.component.css']
 })
-export class CollectiveselectComponent implements OnInit {
+export class CollectiveselectComponent implements OnInit, OnDestroy, AfterViewInit, AfterContentInit, ControlValueAccessor {
 
-  constructor() { }
+  @ViewChild(Dropdown) dropdown: Dropdown;
+  @ContentChild(FormControlName) formControlNameRef: FormControlName;
+  formControl: FormControl;
+  delegatedMethodCalls = new ReplaySubject<(_: ControlValueAccessor) => void>();
+  delegatedMethodsSubscription: Subscription;
+  private destroySubject = new Subject<void>();
+  collectiveSubject = new BehaviorSubject<RawCollective[]>(undefined);
+
+  originalControl = new FormControl(null);
+  choiceControl = new FormControl('');
+
+  formState$: Observable<CollectiveState>;
+  formStateComponent$: Observable<CollectiveState>;
+
+  readonly = false;
+  editable = false;
+
+  @Input()
+  set readOnly(value: boolean) {
+    this.readonly = value;
+  }
+
+  @Input()
+  set editAble(value: boolean) {
+    this.editable = value;
+  }
+
+  group = new FormGroup(
+    {
+      original: this.originalControl,
+      choice: this.choiceControl,
+    },
+    [stateValidator]
+  );
+
+  status: RawCollective[] = new Array(1).fill({id: 0, code: null, title: 'Gruppen', name: null, description: null});
+
+
+  OnChangeWrapper(onChange: (stateIn) => void): (stateOut: RawCollective) => void {
+    return ((state: RawCollective): void => {
+      this.formControl.setValue(state);
+      this.choiceControl.setValue(state);
+      onChange(state.id);
+    });
+  }
+
+  registerOnChange(fn: any): void {
+    this.delegatedMethodCalls.next(accessor => accessor.registerOnChange(this.OnChangeWrapper(fn)));
+  }
+
+  registerOnTouched(fn: any): void {
+    this.delegatedMethodCalls.next(accessor => accessor.registerOnTouched(fn));
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.delegatedMethodCalls.next(accessor => accessor.setDisabledState(isDisabled));
+  }
+
+  writeValue(stateId): void {
+    if (typeof stateId === 'number') {
+      for (const el in this.status) {
+        if (stateId === this.status[el].id) {
+          stateId = this.status[el];
+        }
+      }
+    }
+    this.delegatedMethodCalls.next(accessor => accessor.writeValue(stateId));
+  }
+
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit() {
+    this.formState$ = this.store.select(getCollectiveState);
+
+    this.formStateComponent$ = this.formState$.pipe(
+      takeUntil(this.destroySubject),
+      tap( state => {
+        Object.keys(state.entities).forEach( key => {
+          this.status.push(state.entities[key]);
+        });
+        this.collectiveSubject.next(this.status);
+      }),
+      // shareReplay(),
+      publishReplay(1),
+      refCount()
+    );
+    this.formStateComponent$.subscribe();
+  }
+
+  ngAfterViewInit(): void {
+    this.delegatedMethodsSubscription = this.delegatedMethodCalls.pipe(
+      delay(0),
+    ).subscribe(fn => fn(this.dropdown));
+  }
+
+  ngOnDestroy(): void {
+    if (this.delegatedMethodsSubscription) {
+      this.delegatedMethodsSubscription.unsubscribe();
+    }
+    this.destroySubject.next();
+    this.destroySubject.complete();
+    this.collectiveSubject.complete();
+  }
+
+  ngAfterContentInit(): void {
+    this.formControl = this.formControlNameRef.control;
+    this.originalControl.setValue(this.formControl);
+    this.choiceControl.setValue(this.formControl.value);
   }
 
 }
