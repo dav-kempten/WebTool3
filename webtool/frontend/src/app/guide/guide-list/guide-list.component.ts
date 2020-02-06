f18f2537f3c2459c6862266fc5c3494e5085dbc8
Bueddi <jojo@dav-kempten.de>
2020-02-06 08:36:05 +0100

    introduce new authentification structure for users and staff

--- webtool/frontend/src/app/guide/guide-list/guide-list.component.ts
+++ webtool/frontend/src/app/guide/guide-list/guide-list.component.ts
@@ -2,7 +2,7 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {select, Store} from '@ngrx/store';
 import {AppState, selectRouterFragment} from '../../app.state';
 import {Router} from '@angular/router';
-import {AuthService} from '../../core/service/auth.service';
+import {AuthService, User} from '../../core/service/auth.service';
 import {RequestGuideSummaries} from '../../core/store/guide-summary.actions';
 import {flatMap, map, publishReplay, refCount, takeUntil, tap} from 'rxjs/operators';
 import {BehaviorSubject, Observable, Subject} from 'rxjs';
@@ -20,13 +20,33 @@ export class GuideListComponent implements OnInit, OnDestroy {
   part$: Observable<string>;
   guides$: Observable<GuideSummary[]>;
 
+  authState$: Observable<User>;
+  loginObject = {id: undefined, firstName: '', lastName: '', role: undefined, valState: 0};
   partNewGuide = new BehaviorSubject<string>('');
 
-  constructor(private store: Store<AppState>, private router: Router, private authService: AuthService) {
+  constructor(private store: Store<AppState>, private router: Router, private userService: AuthService) {
     this.store.dispatch(new RequestGuideSummaries());
   }
 
   ngOnInit() {
+
+    this.authState$ = this.userService.user$;
+    this.authState$.pipe(
+      tap(value => {
+        this.loginObject = { ...value, valState: 0 };
+        if (value.role === 'Administrator') {
+          this.loginObject.valState = 4;
+        } else if (value.role === 'Geschäftsstelle') {
+          this.loginObject.valState = 3;
+        } else if (value.role === 'Fachbereichssprecher') {
+          this.loginObject.valState = 2;
+        } else if (value.role === 'Trainer') {
+          this.loginObject.valState = 1;
+        } else { this.loginObject.valState = 0; }
+        console.log(this.loginObject);
+      }),
+    ).subscribe();
+
 
     this.part$ = this.store.pipe(
       takeUntil(this.destroySubject),
@@ -62,7 +82,11 @@ export class GuideListComponent implements OnInit, OnDestroy {
   }
 
   selectGuide(guide): void {
-    this.router.navigate(['trainers', guide.id]);
+    if (!!guide) {
+      if (this.loginObject.valState >= 3 || this.loginObject.id === guide.id) {
+        this.router.navigate(['trainers', guide.id]);
+      }
+    }
   }
 
 }
