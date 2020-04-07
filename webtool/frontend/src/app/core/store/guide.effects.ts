1ad0129165dccd4968cf7164e13f05b4fd9a069f
Bueddi <jojo@dav-kempten.de>
2020-04-07 08:17:07 +0200

    add safe-effect and tranformations for saving and getting guide-data

--- webtool/frontend/src/app/core/store/guide.effects.ts
+++ webtool/frontend/src/app/core/store/guide.effects.ts
@@ -5,11 +5,10 @@ import {Actions, Effect, ofType} from '@ngrx/effects';
 import {Injectable} from '@angular/core';
 import {GuideService} from '../service/guide.service';
 import {AppState} from '../../app.state';
-import {AddGuide, GuideActionTypes, GuideNotModified, RequestGuide} from './guide.actions';
+import {AddGuide, GuideActionTypes, GuideNotModified, RequestGuide, UpdateGuide, UpsertGuide} from './guide.actions';
+import {Guide as RawGuide} from '../../model/guide';
+import {Guide} from './guide.model';
 
-function convertDecimal(rawValue: string): number {
-  return Number(rawValue.replace('.', ''));
-}
 
 @Injectable({
   providedIn: 'root'
@@ -26,9 +25,9 @@ export class GuideEffects {
     map((action: RequestGuide) => action.payload),
     switchMap(payload => {
       return this.guideService.getGuide(payload.id).pipe(
-        map(guideInstance => {
-          if (guideInstance.id !== 0) {
-            return new AddGuide({guide: guideInstance});
+        map(guide => {
+          if (guide.id !== 0) {
+            return new AddGuide({guide: this.transformGuide(guide)});
           } else {
             return new GuideNotModified();
           }
@@ -36,4 +35,78 @@ export class GuideEffects {
       );
     })
   );
+
+  @Effect()
+  safeGuide$: Observable<Action> = this.actions$.pipe(
+    ofType<UpsertGuide>(GuideActionTypes.UpsertGuide),
+    map((action: UpsertGuide) => action.payload),
+    switchMap(payload  => {
+      return this.guideService.upsertGuide(this.transformGuideForSaving(payload.guide)).pipe(
+        map(guide => {
+          if (guide.id !== 0) {
+            alert('Trainer erfolgreich gespeichert.');
+            return new UpdateGuide({guide: {
+              id: guide.id,
+              changes: {...guide}}});
+          } else {
+            alert('Trainer speichern gescheitert, nocheinmal versuchen oder Seite neuladen.');
+            return new GuideNotModified();
+          }
+        })
+      );
+    })
+  );
+
+  transformGuide(guide: RawGuide): Guide {
+
+    let profileCity = '';
+    let profileJob = '';
+    let profileName = '';
+    let profileQualification = '';
+    let profileReason = '';
+    let profileHobby = '';
+    let profileTip = '';
+
+    if (guide.profile !== null) {
+      profileCity = JSON.parse(guide.profile).city;
+      profileJob = JSON.parse(guide.profile).job;
+      profileName = JSON.parse(guide.profile).name;
+      profileQualification = JSON.parse(guide.profile).qualification;
+      profileReason = JSON.parse(guide.profile).reason;
+      profileHobby = JSON.parse(guide.profile).hobby;
+      profileTip = JSON.parse(guide.profile).tip;
+    }
+
+    delete guide.profile;
+
+    return {
+      ... guide,
+      profileCity,
+      profileJob,
+      profileName,
+      profileQualification,
+      profileReason,
+      profileHobby,
+      profileTip
+    };
+  }
+
+  transformGuideForSaving(guide: Guide): RawGuide {
+    const profile = JSON.stringify({city: guide.profileCity, hobby: guide.profileHobby, job: guide.profileJob,
+      name: guide.profileName, qualification: guide.profileQualification, reason: guide.profileReason, tip: guide.profileTip
+    });
+
+    delete guide.profileCity;
+    delete guide.profileHobby;
+    delete guide.profileJob;
+    delete guide.profileName;
+    delete guide.profileQualification;
+    delete guide.profileReason;
+    delete guide.profileTip;
+
+    return {
+      ... guide,
+      profile
+    };
+  }
 }
