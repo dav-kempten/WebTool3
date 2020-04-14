cbc3090d0a2bdeadc613ede70438d96183f20eed
Bueddi <jojo@dav-kempten.de>
2020-04-14 10:26:48 +0200

    add retraining update & validiation

--- webtool/server/serializers/frontend/retrainings.py
+++ webtool/server/serializers/frontend/retrainings.py
@@ -1,7 +1,18 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
-from server.models import Retraining, Qualification
+from server.models import Retraining, UserQualification
+
+def update_retraining(instance, validated_data, context):
+    instance.qualification = validated_data.get('qualification', instance.qualification)
+    instance.year = validated_data.get('year', instance.year)
+    instance.specific = validated_data.get('specific', instance.specific)
+    instance.description = validated_data.get('description', instance.description)
+    instance.note = validated_data.get('note', instance.note)
+
+    instance.save()
+
+    return instance
 
 
 class RetrainingSerializer(serializers.ModelSerializer):
@@ -9,12 +20,12 @@ class RetrainingSerializer(serializers.ModelSerializer):
         source='pk', queryset=Retraining.objects.all(), default=None, allow_null=True
     )
     qualification = serializers.PrimaryKeyRelatedField(
-        queryset=Qualification.objects.all(), default=None, allow_null=True
+        queryset=UserQualification.objects.all(), default=None, allow_null=True
     )
     year = serializers.IntegerField()
     specific = serializers.BooleanField(default=False)
     description = serializers.CharField(default='')
-    note = serializers.CharField()
+    note = serializers.CharField(allow_blank=True)
 
     class Meta:
         model = Retraining
@@ -25,3 +36,20 @@ class RetrainingSerializer(serializers.ModelSerializer):
                   'description',
                   'note'
                   )
+
+        def validate(self, data):
+            if self.instance is not None:
+                # This is the update case
+                user_qualification = self.instance
+
+                instance_data = data.get('pk')
+
+                if instance_data is None:
+                    raise serializers.ValidationError("instance Id is missing")
+                elif instance_data.pk != user_qualification.pk:
+                    raise serializers.ValidationError("Wrong instance Id")
+
+            return data
+
+        def update(self, instance, validated_data):
+            update_retraining(instance, validated_data, self.context)
