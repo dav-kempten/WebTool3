0d28cca838ff36f279c5ed5af4c4f19da7fa3530
Wolfgang Doll <wolfgang.doll@web.de>
2020-08-08 09:58:59 +0200

    Enable FreeClimber update as well

--- webtool/server/management/commands/replication.py
+++ webtool/server/management/commands/replication.py
@@ -8,55 +8,74 @@ from server.models import Reference
 
 
 class Command(BaseCommand):
-    help = 'Update data base with data from the DAV KV_Manager *.csv file'
+    help = 'Update data base with data from DAV KV_Manager or FreeClimber *.csv file'
 
     def __init__(self, *args, **kwargs):
         super(Command, self).__init__(*args, **kwargs)
         self.log = None
 
-    def update(self, kvm):
-        csv_file = csv.DictReader(kvm, dialect='excel', delimiter=';')
-        for row in csv_file:
+    def update(self, csv_file):
+        csv_dict = csv.DictReader(csv_file, dialect='excel', delimiter=';')
+        for row in csv_dict:
 
-            reference_code = row["Kursnummer"]
+            reference_code = row.get("Kursnummer")
+            if reference_code is None:
+                kvm = False
+                reference_code = row.get("Nr")
+            else:
+                kvm = True
+            if reference_code is None:
+                continue
 
             try:
                 reference = Reference.get_reference(reference_code)
             except Reference.DoesNotExist:
                 continue
 
-            print(reference_code)
-
-            cur_quantity = int(row["GebuchteTN"])
+            if reference.deprecated:
+                continue
 
             event = reference.event
+            if event is None:
+                continue
+
+            print(reference_code)
+
+            if kvm:
+                cur_quantity = int(row["GebuchteTN"])
+            else:
+                cur_quantity = int(row["Ist Teilnehmer"])
+
+            updated = False
             if hasattr(event, 'tour') and event.tour:
                 tour = event.tour
                 cq = tour.cur_quantity
                 if cq != cur_quantity:
                     tour.cur_quantity = cur_quantity
                     tour.save()
-                    print(reference_code, "updated")
+                    updated = True
             if hasattr(event, 'talk') and event.talk:
                 talk = event.talk
                 cq = talk.cur_quantity
                 if cq != cur_quantity:
                     talk.cur_quantity = cur_quantity
                     talk.save()
-                    print(reference_code, "updated")
+                    updated = True
             if hasattr(event, 'meeting') and event.meeting:
                 instruction = event.meeting
                 cq = instruction.cur_quantity
-                cm = instruction.max_quantity
                 if cq != cur_quantity:
                     instruction.cur_quantity = cur_quantity
                     instruction.save()
-                    print(reference_code, "updated")
+                    updated = True
+
+            if updated:
+                print(reference_code, "updated")
 
     def add_arguments(self, parser):
         parser.add_argument('data', type=str)
 
     def handle(self, *args, **options):
         path = options['data']
-        with io.open(path, 'r', encoding='latin-1') as kvm_csv:
-            self.update(kvm_csv)
+        with io.open(path, 'r', encoding='latin-1') as csv_file:
+            self.update(csv_file)
