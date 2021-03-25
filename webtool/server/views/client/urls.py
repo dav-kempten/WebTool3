9a5a3fe83cf9d693e27cb296959eef82773b8de8
Bueddi <johannes.buettner@stud.hs-kempten.de>
2021-03-25 17:45:26 +0100

    rename variable of router.register

--- webtool/server/views/client/urls.py
+++ webtool/server/views/client/urls.py
@@ -3,8 +3,8 @@ from rest_framework.routers import DefaultRouter
 from . import GuideViewSet, ActivityViewSet, TopicViewSet, CollectiveViewSet, BookletViewSet
 
 router = DefaultRouter()
-router.register(r'guides', GuideViewSet, base_name='guides')
-router.register(r'activities', ActivityViewSet, base_name='activities')
-router.register(r'topics', TopicViewSet, base_name='topics')
-router.register(r'collectives', CollectiveViewSet, base_name='collectives')
-router.register(r'booklets', BookletViewSet, base_name='booklets')
+router.register(r'guides', GuideViewSet, basename='guides')
+router.register(r'activities', ActivityViewSet, basename='activities')
+router.register(r'topics', TopicViewSet, basename='topics')
+router.register(r'collectives', CollectiveViewSet, basename='collectives')
+router.register(r'booklets', BookletViewSet, basename='booklets')
