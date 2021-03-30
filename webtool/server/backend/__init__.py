75e9451a3310e82053b98418cd0f053efe623478
Bueddi <johannes.buettner@stud.hs-kempten.de>
2021-03-30 17:13:17 +0200

    change custom authenticate for member_id login

--- webtool/server/backend/__init__.py
+++ webtool/server/backend/__init__.py
@@ -7,9 +7,9 @@ from server.models import Profile, Tour, Instruction, Session, Talk
 
 class Backend(ModelBackend):
 
-    def authenticate(self, member_id=None, **kwargs):
+    def authenticate(self, request, username=None, password=None, **kwargs):
         try:
-            profile = Profile.objects.get(member_id=member_id)
+            profile = Profile.objects.get(member_id=kwargs.get('member_id'))
         except Profile.DoesNotExist:
             return None
 
