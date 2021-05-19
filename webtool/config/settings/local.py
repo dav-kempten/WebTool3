edc319d49e1f27a0a92a198cf4c4b5578728565c
Bueddi <jojo@dav-kempten.de>
2021-05-19 18:02:12 +0200

    change place for static root and project dir

--- webtool/config/settings/local.py
+++ webtool/config/settings/local.py
@@ -7,5 +7,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '0.0.0.0']
 
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
 # INSTALLED_APPS += ("debug_toolbar", )
 
