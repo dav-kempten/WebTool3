98d490a31ccf0ab1c84313792b05200fe1711a3f
Bueddi <johannes.buettner@stud.hs-kempten.de>
2021-05-28 12:59:45 +0200

    add empty component for guide-detail

---
+++ webtool/frontend/src/app/guide/guide-detail/guide-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'avk-guide-detail',
+  templateUrl: './guide-detail.component.html',
+  styleUrls: ['./guide-detail.component.css']
+})
+export class GuideDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
