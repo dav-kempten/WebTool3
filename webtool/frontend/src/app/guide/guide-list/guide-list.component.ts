1a77988ab8bcb33bd2edb3b262491fa87923237a
Bueddi <johannes.buettner@stud.hs-kempten.de>
2021-05-28 12:59:35 +0200

    add empty component for guide-list

---
+++ webtool/frontend/src/app/guide/guide-list/guide-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'avk-guide-list',
+  templateUrl: './guide-list.component.html',
+  styleUrls: ['./guide-list.component.css']
+})
+export class GuideListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
