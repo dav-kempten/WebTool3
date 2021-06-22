4fe3982937d9bc79858057d19db8178991e9cbe6
Bueddi <johannes.buettner@stud.hs-kempten.de>
2021-06-22 16:41:54 +0200

    enable prototyp of new permission handling in instruction-detail

--- webtool/frontend/src/app/instruction/instruction-detail/instruction-detail.component.ts
+++ webtool/frontend/src/app/instruction/instruction-detail/instruction-detail.component.ts
@@ -16,13 +16,13 @@ import {getInstructionById} from '../../core/store/instruction.selectors';
 import {Instruction} from '../../core/store/instruction.model';
 import {filter, flatMap, map, publishReplay, refCount, takeUntil, tap} from 'rxjs/operators';
 import {getEventsByIds} from '../../core/store/event.selectors';
-import {AuthService, User} from '../../core/service/auth.service';
+import {AuthService} from '../../core/service/auth.service';
 import {Event} from '../../model/event';
 import {Category, Topic} from '../../model/value';
 import {FormArray, FormControl, FormGroup} from '@angular/forms';
 import {UpdateEvent} from '../../core/store/event.actions';
 import {ConfirmationService} from 'primeng/api';
-import {Permission, PermissionService} from '../../core/service/permission.service';
+import {Permission, PermissionLevel} from '../../core/service/permission.service';
 
 @Component({
   selector: 'avk-instruction-detail',
@@ -31,11 +31,6 @@ import {Permission, PermissionService} from '../../core/service/permission.servi
 })
 
 export class InstructionDetailComponent implements OnInit, OnDestroy {
-  private myObserver = {
-    next: (x: Permission) => console.log('Observer got a next value: ' + x.permissionLevel + ' ' + x.guideId),
-    error: (err: Error) => console.error('Observer got an error: ' + err),
-    complete: () => console.log('Observer got a complete notification'),
-  };
 
   private destroySubject: Subject<boolean> = new Subject<boolean>();
   private instructionSubject = new BehaviorSubject<FormGroup>(undefined);
@@ -62,10 +57,7 @@ export class InstructionDetailComponent implements OnInit, OnDestroy {
   events$: Observable<Event[]>;
   category$: Observable<Category>;
 
-  userIsStaff$: Observable<boolean>;
-  userIsAdmin$: Observable<boolean>;
-  userIsOwner$: Observable<boolean> = this.instructionOwner.asObservable();
-  userCurrent$: Observable<number>;
+  permissionHandler$: Observable<boolean>;
   permissionCurrent$: Observable<Permission>;
 
   display = false;
@@ -75,12 +67,7 @@ export class InstructionDetailComponent implements OnInit, OnDestroy {
   constructor(private store: Store<AppState>, private userService: AuthService, private confirmationService: ConfirmationService) { }
 
   ngOnInit(): void {
-    this.userIsStaff$ = this.userService.isStaff$;
-    this.userIsAdmin$ = this.userService.isAdministrator$;
-
-    this.userCurrent$ = this.userService.guideId$;
     this.permissionCurrent$ = this.userService.guidePermission$;
-    this.permissionCurrent$.subscribe(this.myObserver);
 
     this.instructionId$ = this.store.select(selectRouterDetailId);
 
@@ -94,10 +81,18 @@ export class InstructionDetailComponent implements OnInit, OnDestroy {
             this.store.dispatch(new RequestInstruction({id}));
           } else {
             /* Check if current user is owner of instruction */
-            this.userCurrent$.pipe(
+            this.permissionHandler$ = this.permissionCurrent$.pipe(
               takeUntil(this.destroySubject),
-              tap(value => this.instructionOwner.next(instruction.guideId === value))
-            ).subscribe();
+              map(permission => {
+                this.instructionOwner.next(permission.guideId === instruction.guideId);
+                if (permission.permissionLevel >= PermissionLevel.coordinator) {
+                  return true;
+                } else if (permission.permissionLevel === PermissionLevel.guide) {
+                  return permission.guideId === instruction.guideId;
+                }
+                return false;
+              })
+            );
             /* Generate instruction */
             const instructionGroup = instructionGroupFactory(instruction);
             instructionGroup.valueChanges.pipe(
@@ -190,7 +185,6 @@ export class InstructionDetailComponent implements OnInit, OnDestroy {
     );
 
     this.instructionId$.subscribe();
-    this.instruction$.subscribe();
     this.topic$.subscribe();
     this.category$.subscribe();
     this.eventIds$.subscribe();
