fbb1bbc78d378ccf60712764017a1de677db0a2e
Bueddi <jojo@dav-kempten.de>
2021-09-10 10:53:08 +0200

    add filter for guide in guide- and team-field for instruction and tours

--- webtool/server/admin_filters.py
+++ webtool/server/admin_filters.py
@@ -1,7 +1,8 @@
 from django.contrib.admin import SimpleListFilter
 from django.utils.translation import gettext_lazy as _
+from django.db.models import Q
 
-from server.models import Qualification
+from server.models import Qualification, Guide
 
 
 class QualificationFilter(SimpleListFilter):
@@ -22,3 +23,18 @@ class QualificationFilter(SimpleListFilter):
     def queryset(self, request, queryset):
         if self.value():
             return queryset.filter(qualification_list__qualification__code=self.value())
+
+class GuideTeamFilter(SimpleListFilter):
+    title = _('Veranstaltungsleitung oder Team')
+    parameter_name = 'Guide'
+
+    tuple_list=[]
+    for query in Guide.objects.all():
+        tuple_list.append((query.pk, _(query.name)))
+
+    def lookups(self, request, model_admin):
+        return self.tuple_list
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(Q(guide__user__pk=self.value()) | Q(team__user__pk=self.value()))
