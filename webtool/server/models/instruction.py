484d752b6d509e56e7039f753baa49329f1c58d6
Bueddi <johannes.buettner@dav-kempten.de>
2025-11-28 11:42:03 +0100

    Add little adjustment for theory parts

--- webtool/server/models/instruction.py
+++ webtool/server/models/instruction.py
@@ -162,8 +162,8 @@ class Instruction(TimeMixin, GuidedEventMixin, AdminMixin, AdmissionMixin, Chapt
             output.write('<p><strong>Weitere Termine:</strong><br />')
             output.write('<br />'.join([e.appointment() for e in self.meeting_list.all()]))
             output.write('<br /><br />')
-            output.write('Alle Theorieteile sind verpflichtend und Voraussetzung für die Teilnahme an den '
-                         'Praxisterminen. Die Theorieteile müssen in Präsenz wahrgenommen werden, '
+            output.write('<strong>Obacht!<\strong> Alle Theorieteile sind verpflichtend und Voraussetzung für die '
+                         'Teilnahme an den Praxisterminen. Die Theorieteile müssen in Präsenz wahrgenommen werden, '
                          'sofern nichts anderes in der Beschreibung steht.')
             output.write('</p>')
 
